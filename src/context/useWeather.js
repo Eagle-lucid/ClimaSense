7c62463d2e3ed5488e9207fd0e5f48f47654ef75
Eagle-lucid <vicokefie@gmail.com>
2025-08-17 11:20:22 -0400

    feat: add useWeather hook for accessing WeatherContext in components

---
+++ src/context/useWeather.js
@@ -0,0 +1,7 @@
+// src/context/useWeather.js 
+import { useContext } from "react";
+import { WeatherContext } from "./WeatherContext";
+
+const useWeather = () => useContext(WeatherContext);
+
+export default useWeather
