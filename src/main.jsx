9a1fbacb22cabc2974e45b298bd234e98c249db4
Eagle-lucid <vicokefie@gmail.com>
2025-08-24 01:52:30 -0400

    refactor: update main entry to support new context and app structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 /* IMPORTS */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import APP from './App.jsx';
+import App from './App.jsx';
 import './styles/App.scss';
 
 /* RENDER */
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode> 
-    <APP />
-    </React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
